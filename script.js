2bea0f11439a9cb208c87f6b69f9304a0ace6d37
WebDev-Ayush-Rawat <rawat.ayush.code@gmail.com>
2025-09-13 09:30:45 +0530

    HTML done and added CSS file to add styles

---
+++ script.js
@@ -0,0 +1,17 @@
+// ----------------RIPPLE EFFECT//
+
+document.querySelectorAll('.btn').forEach(btn => {
+
+  btn.addEventListener('click',(e) =>{
+
+    const ripple = document.createElement('span');
+    ripple.className = 'ripple';
+    const rect = btn.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    ripple.style.width = ripple.style.height = size + 'px';
+    ripple.style.left = (e.clientX - rect.left - size/2) + 'px';
+    ripple.style.top = (e.clientY - rect.top - size/2) + 'px';
+    btn.appendChild(ripple);
+    ripple.addEventListener('animationend', () => ripple.remove());
+  });
+});
